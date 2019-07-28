75f5fa34b6540435f19b90a8033ed4c32ba051f9
Chakradhar-Battili <53245393+Chakradhar-Battili@users.noreply.github.com>
2019-07-28 22:59:42 +0530

    made ui changes
    ui changes added

---
+++ employee-portal-ui/src/app/components/employees/employees.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html'
+})
+export class EmployeesComponent implements OnInit {
+  public employeelist;
+
+  constructor(private employeeService : EmployeeService) { }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  getEmployees() {
+    this.employeeService.getEmployees().subscribe(
+      data => { this.employeelist = data},
+      err => console.error(err),
+      () => console.log('Employee details fetched..')
+    );
+  }
+
+}
