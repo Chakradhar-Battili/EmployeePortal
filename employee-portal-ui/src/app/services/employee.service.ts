2f8815625f9eca8f3a11bf22466d069fd1c2e7cb
Chakradhar <chakradhar.aug28@gmail.com>
2019-07-29 15:48:40 +0530

    made url changes

--- employee-portal-ui/src/app/services/employee.service.ts
+++ employee-portal-ui/src/app/services/employee.service.ts
@@ -12,11 +12,11 @@ export class EmployeeService {
   constructor(private http:HttpClient) { }
 
   getEmployees(): Observable<any> {
-    return this.http.get('fetch');
+    return this.http.get('employee/fetch');
   }
 
   createEmployeeRegistration(employee) {
     let body = JSON.stringify(employee);
-    return this.http.post('create',body,httpOptions);
+    return this.http.post('employee/create',body,httpOptions);
   }
 }
